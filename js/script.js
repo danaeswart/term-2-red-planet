07641b2379cb2a9d8eedd3dd431537709875708e
danaeswart <241022@virtualwindow.co.za>
2024-05-21 13:24:11 +0200

    trying out a new popup

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//  popup for the book flights page-- your flight was added to the chart//
+
+
+let popupSubmit= document.getElementById("popupSubmit");
+
+function openPopupSubmit(){
+    popupSubmit.classList.add("open-popup");
+}
+
+function closePopupSubmit(){
+    popupSubmit.classList.remove("open-popup");
+}
