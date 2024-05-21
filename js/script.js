55c907f7ca580596b28478fa98c9cb360ce9fb33
danaeswart <241022@virtualwindow.co.za>
2024-05-21 14:21:30 +0200

    added to chart popup addd
    yayay i love it! i think i can add some animation later on, but it looks good for a popupi

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
-//  popup for the book flights page-- your flight was added to the chart//
+
+// popup for the book flights page-- your flight was added to the chart//
+
+    function openPopupSubmit(){
+        //setting a delay timer- set to 300ms -- opens popup when someone books a flight
+        setTimeout(() => {  
+            document.getElementById("popup").style.display="block";
+        
+        }, 250);
+    
+
+    }
+    // adding animations into my onclick popup----
+    // it does move up the screen then doesnt want to reactivate
+
+    // let id=null;
+    // function moveAnimation(){
+    //     let element= document.getElementById("popup");
+    //     let pos=250;
+    //     clearInterval(id);
+    //     id= setInterval(frameFunction,5);
+    //     function frameFunction(){
+    //         if (pos ==-200){
+    //             clearInterval(id);
+    //         } else{
+    //             pos--;
+    //             element.style.top=pos+'px';
+    //         }
+
+        
+    //     }
+        
+    // }
 
 
-let popupSubmit= document.getElementById("popupSubmit");
 
-function openPopupSubmit(){
-    popupSubmit.classList.add("open-popup");
-}
+    function closePopupSubmit(){
+        // this function closes the pop up window
+        // moveAnimation();
+        document.getElementById("popup").style.display="none";
+    
+        
+    }
 
-function closePopupSubmit(){
-    popupSubmit.classList.remove("open-popup");
-}
+
+// END OF CODE FOR POPUP ON FLIGHTS PAGE 
