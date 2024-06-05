2ccac62f5182182bda4ee724311eb95edd923fb1
danaeswart <241022@virtualwindow.co.za>
2024-06-05 15:18:52 +0200

    total update works- counts the total

--- js/script.js
+++ js/script.js
@@ -49,23 +49,48 @@
 
 //----------remove button -------
 let removeItem= document.getElementsByClassName("removeItembtn");  //stores the buttons as an array
-console.log(removeItem);
+//console.log(removeItem);
 
 for(let i =0; i<removeItem.length;i++){  //loop through all your remove buttons
     var buttonInArr= removeItem[i]; //the button we are currently on
     buttonInArr.addEventListener('click',function(event){ //listens for click event- when the btn is clicked-finds the button you clicked
         let btnClicked = event.target;   // get the btn that was clciked
         btnClicked.parentElement.parentElement.remove();  //delete the whole row
+        updateTotal();// update the removed item
     })
 
 
 }
 
+updateTotal();
 
 //----------function for updating total
 function updateTotal(){
+
+    let total=0;
+
+    let arrOfCartItemPrice = document.getElementsByClassName("priceInRand");// create arr for all the price <P>price</P>
+
+    //loop through all the prices
+    for(let i =0; i<arrOfCartItemPrice.length;i++){
+        let stringPrice= arrOfCartItemPrice[i].innerHTML; //grab the value for the <p></p>
+       
+        let noR= stringPrice.replace("R ",'');  //remove R
+        let tempPrice=noR.replace(',','');  //remove ,
+  
+
+         let itemPrice= parseFloat(tempPrice); // turn string to int.
+         total+=itemPrice;  //total price
+         
+    }
+
+    let totalString= "R "+total;
+    document.getElementById("totalAmount").innerHTML= totalString;// display total 
+
     
+
 }
+
 
 
 //////////////////////////// making variables that will be used and stored on the contact page///////////
